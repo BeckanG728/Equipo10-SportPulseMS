9084bd9256e55d99244672c6db18b3f1404a0e7e
BeckanG728 <209495631+BeckanG728@users.noreply.github.com>
2026-04-18 16:08:25 -0500

    refactor: respuesta del error 503, generacion del token aÃgregando username

--- ms-gateway/src/main/java/es/bytescolab/ms_gateway/controller/FallbackController.java
+++ ms-gateway/src/main/java/es/bytescolab/ms_gateway/controller/FallbackController.java
@@ -1,33 +1,16 @@
 package es.bytescolab.ms_gateway.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import es.bytescolab.ms_gateway.exception.domain.ServiceUnavailableException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.time.Instant;
-import java.util.Map;
-
 @RestController
 public class FallbackController {
 
     @RequestMapping("/fallback/{service}")
-    public Mono<ResponseEntity<Map<String, Object>>> fallback(
-            @PathVariable String service) {
-
-        Map<String, Object> body = Map.of(
-                "code", "SERVICE_UNAVAILABLE",
-                "service", service,
-                "message", "Service is down",
-                "timestamp", Instant.now().toString()
-        );
-
-        return Mono.just(
-                ResponseEntity
-                        .status(HttpStatus.SERVICE_UNAVAILABLE)
-                        .body(body)
-        );
+    public Mono<Void> fallback(@PathVariable String service) {
+        return Mono.error(new ServiceUnavailableException(service));
     }
 }
