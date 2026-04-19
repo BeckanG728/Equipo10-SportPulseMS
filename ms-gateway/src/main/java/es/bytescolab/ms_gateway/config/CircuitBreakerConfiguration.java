5e86ee8785833408f3aa89f4dfa2a74f0a18899c
BeckanG728 <209495631+BeckanG728@users.noreply.github.com>
2026-04-19 00:29:53 -0500

    gateway: implementacion de rate limiting por IP

--- ms-gateway/src/main/java/es/bytescolab/ms_gateway/config/CircuitBreakerConfiguration.java
+++
@@ -1,8 +0,0 @@
-package es.bytescolab.ms_gateway.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CircuitBreakerConfiguration {
-    // Configuración manejada por application.yaml
-}
---
+++ ms-gateway/src/main/java/es/bytescolab/ms_gateway/filter/RequestRateLimiter.java
@@ -0,0 +1,18 @@
+package es.bytescolab.ms_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Component
+public class RequestRateLimiter {
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress()
+        );
+    }
+}
