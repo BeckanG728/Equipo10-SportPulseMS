7fad184a4ed25662bbedcaa64d3b3e33760ed577
BeckanG728 <209495631+BeckanG728@users.noreply.github.com>
2026-04-19 20:19:05 -0500

    feat(auth): implementado endpoint POST api/auth/validate interno

--- ms-auth/src/main/java/es/bytescolab/ms_auth/config/SecurityConfig.java
+++ ms-auth/src/main/java/es/bytescolab/ms_auth/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register")
+                        .requestMatchers("/auth/login", "/auth/register", "/auth/validate")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
